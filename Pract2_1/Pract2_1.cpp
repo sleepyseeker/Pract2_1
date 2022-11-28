8b7b5a731a6a031d3d9076af24fa746919f1551e
sleepyseeker <bbilopolskiy@gmail.com>
2022-11-28 23:20:41 +0200

    Initial commit

---
+++ Pract2_1/Pract2_1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a, b, c;
+    a = 0; b = 0; c = 0;
+
+    printf("Enter a:");
+    scanf("%i", &a);
+
+    printf("Enter b:");
+    scanf("%i", &b);
+
+    printf("Enter c:");
+    scanf("%i", &c);
+
+    if (a + b >= 0)
+        printf("summ a+b>0\n");
+    if (a + c >= 0)
+        printf("summ a+c>0\n");
+    if (b + c >= 0)
+        printf("summ b+c>0\n");
+    system("pause");
+    return 0;
+}
